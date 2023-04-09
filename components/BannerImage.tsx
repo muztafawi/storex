5cd436d7ab24f0cdb927ca872a48d646f4a2bc49
Amin Mustafa <muztafawi@gmail.com>
2023-04-09 22:04:01 +0300

    Added main part structure

--- components/BannerImage.tsx
+++ components/BannerImage.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BannerImage = () => {
   return (
-    <div className="bg-[#d9d9d9] w-full h-60 mt-10">
+    <div className="bg-[#d9d9d9] w-full h-60 mt-9">
       <div></div>
     </div>
   );
